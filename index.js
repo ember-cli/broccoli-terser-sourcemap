d516d6775ac284e6daf77441a677d8a8512342a2
Sandesh <sandy2557@gmail.com>
2015-06-18 15:54:23 -0700

    dropping conditional as per suggestion

--- index.js
+++ index.js
@@ -98,7 +98,7 @@ UglifyWriter.prototype.processFile = function(inFile, outFile, relativePath, out
   try {
     var result = UglifyJS.minify(src, merge(opts, this.options));
   } catch(e) {
-    e.filename = (!e.filename || e.filename === '?') ? relativePath : e.filename;
+    e.filename = relativePath;
     throw e;
   }
 
